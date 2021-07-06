34f306bef4e9444a61cc73c5d20d6dc16c2bc143
jiangyue <11849176@mail.sustech.edu.cn>
2021-07-06 21:10:10 +0800

    add bus timetable
    添加了所有的时刻表

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,6 +12,11 @@ Page({
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
   },
   // 事件处理函数
+  testPage() {
+    wx.navigateTo({
+      url: '../sustc-bus'
+    })
+  },
   bindViewTap() {
     wx.navigateTo({
       url: '../logs/logs'
